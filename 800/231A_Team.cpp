d5d87b72591cecb880faae624b923fa32eef8301
Tarun Vir Singh <tarunvirsingh8@gmail.com>
2026-08-09 13:21:06 +0530

    Create 231A_Team.cpp

---
+++ 800/231A_Team.cpp
@@ -0,0 +1,28 @@
+/*
+Problem: Team (Codeforces 231A)
+Topic: Implementation
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count=0;
+  
+    while(n--){
+        int p, v, t;
+        cin >> p >> v >> t;
+
+        if(p+v+t>=2){
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
