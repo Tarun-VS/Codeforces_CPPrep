df6fee1183185d40ed786c4e447a87a28e7bb400
Tarun Vir Singh <tarunvirsingh8@gmail.com>
2026-08-13 22:38:21 +0530

    Create 112A_Petya_and_Strings.cpp

---
+++ 800/112A_Petya_and_Strings.cpp
@@ -0,0 +1,35 @@
+/*
+Problem: Petya and Strings (Codeforces 112A)
+Topic: strings, implementation
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++) {
+        a[i] = tolower(a[i]);
+        b[i] = tolower(b[i]);
+    }
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] > b[i]) {
+            cout << 1;
+            return 0;
+        }
+        else if (a[i] < b[i]) {
+            cout << -1;
+            return 0;
+        }
+    }
+
+    cout << 0;
+
+    return 0;
+}
