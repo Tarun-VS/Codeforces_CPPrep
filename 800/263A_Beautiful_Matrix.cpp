2d2daa2e834ef1c47fbc6cbe2d71eabf4af934c4
Tarun Vir Singh <tarunvirsingh8@gmail.com>
2026-08-13 22:39:47 +0530

    Create 263A_Beautiful_Matrix.cpp

---
+++ 800/263A_Beautiful_Matrix.cpp
@@ -0,0 +1,31 @@
+/*
+Problem: Beautiful Matrix (Codeforces 263A)
+Topic: 2D arrays, implementation
+
+Time Complexity: O(25)
+Space Complexity: O(25)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[5][5];
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (a[i][j] == 1) {
+                cout << abs(i - 2) + abs(j - 2);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
