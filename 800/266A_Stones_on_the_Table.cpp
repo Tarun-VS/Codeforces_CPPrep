ce723b19e00a84406823a845f5a8a716dda8282d
Tarun Vir Singh <tarunvirsingh8@gmail.com>
2026-08-13 23:04:01 +0530

    Create 266A_Stones_on_the_Table.cpp

---
+++ 800/266A_Stones_on_the_Table.cpp
@@ -0,0 +1,28 @@
+/*
+Problem: Stones on the Table (Codeforces 266A)
+Topic: strings, implementation
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+
+    cin >> n >> s;
+
+    int ans = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1])
+            ans++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
