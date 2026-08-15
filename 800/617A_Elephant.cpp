feec7bece88bddf3e8ad1fe64c2515780fc04dcf
Tarun Vir Singh <tarunvirsingh8@gmail.com>
2026-08-15 22:06:47 +0530

    Create 617A_Elephant.cpp

---
+++ 800/617A_Elephant.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, res = 0;
+    cin >> n;
+
+    res = n / 5;
+
+    if (n % 5 != 0){
+        res++;
+    }
+    
+    cout << res << endl;
+
+    return 0;
+}
