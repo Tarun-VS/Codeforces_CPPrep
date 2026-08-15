3eeef1483117037ab31a32c5a6261aa687ba643c
Tarun Vir Singh <tarunvirsingh8@gmail.com>
2026-08-15 22:44:08 +0530

    Create 50A_Domino_Piling.cpp

---
+++ 800/50A_Domino_Piling.cpp
@@ -0,0 +1,19 @@
+/*
+Problem: Domino Piling (Codeforces 50A)
+Topic: math, implementation
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    cout << (m * n) / 2;
+
+    return 0;
+}
